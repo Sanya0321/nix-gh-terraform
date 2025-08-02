2b2dfd48cd13551fa54d099b10ef94a96dc2d538
Sanya0321 <64781900+Sanya0321@users.noreply.github.com>
2025-08-02 09:34:12 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "r3ap3rpybackup"
-    key = "r3ap3rpybackup/statefolder/statefile"
-    region = "eu-north-1"
-    encrypt = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "r3ap3rpybackup"
+#    key = "r3ap3rpybackup/statefolder/statefile"
+#    region = "eu-north-1"
+#    encrypt = true
+#  }
+#}
